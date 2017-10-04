0d99c4c00f5c85413ec7a765615ba427fa070dc6
Mark C Allen <mark@markcallen.com>
2017-10-03 22:11:07 -0700

    Globals are set if result is present.  Replace variables in command with global value

--- process.go
+++ process.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Process : groups of tasks that need to be run
 type Process struct {
@@ -9,9 +12,26 @@ type Process struct {
 }
 
 // Run : runs the tasks in the Process
-func (p *Process) Run(g []string) {
+func (p *Process) Run(g map[string]string) {
 	fmt.Printf("Process: %s\n", p.Name)
 	for _, task := range p.Tasks {
-		task.Run()
+
+		vars := make([]string, 0, len(g)*2)
+		for v := range g {
+			vars = append(vars, "${"+v+"}", g[v])
+		}
+
+		replacer := strings.NewReplacer(vars...)
+		task.Command = replacer.Replace(task.Command)
+
+		if task.Result != "" {
+			_, ok := g[task.Result]
+			if !ok {
+				fmt.Printf("No global variable %s to set\n", task.Result)
+			}
+			g[task.Result] = task.Run()
+		} else {
+			task.Run()
+		}
 	}
 }
